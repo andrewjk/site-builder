1b7a358cbbdbe5c704489a3cbaa88b26e555cc27
Andrew K <andrewjk@gmail.com>
2019-05-01 15:18:49 +1000

    Added FontAwesome for icons

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,11 +5,26 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus
+} from '@fortawesome/free-solid-svg-icons'
+// import {
+// } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 // Normalize.css
 // HACK: We have to refer to its location explicitly to avoid import errors
 require('../../node_modules/normalize.css/normalize.css')
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+// Add the FontAwesome icons that we will use
+library.add(faPlus)
+
+Vue.component('fa', FontAwesomeIcon)
+
+if (!process.env.IS_WEB) {
+  Vue.use(require('vue-electron'))
+}
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
