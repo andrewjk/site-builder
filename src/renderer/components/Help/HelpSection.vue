4b3647b01b2fb91d95aa0286150978733abc892b
Andrew K <andrewjk@gmail.com>
2019-05-01 15:20:02 +1000

    Added some help pages

---
+++ src/renderer/components/Help/HelpSection.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div class="help-container">
+    <settings-help v-if="section === 'Settings'"></settings-help>
+  </div>
+</template>
+
+<script>
+  import SettingsHelp from './SettingsHelp'
+
+  export default {
+    components: { SettingsHelp },
+    props: {
+      section: ''
+    }
+  }
+</script>
+
+<style scoped>
+  .help-container {
+    padding: 20px;
+  }
+</style>
---
+++ src/renderer/components/Help/SettingsHelp.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div class="help-wrapper">
+    <div class="title">Settings</div>
+    <p>
+      Settings affect the whole of your site, whether it be the icon and title shown at the top of the
+      browser, the description text that search engines will use to find your site, or the fonts and
+      colors used to display your pages.
+    </p>
+    <p>
+      Settings can often be overridden by settings on a page.
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+  .title {
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
+</style>
