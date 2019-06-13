f28674136beb4c26a0071a7af2f6275b98528750
Andrew K <andrewjk@gmail.com>
2019-06-13 10:41:03 +1000

    Don't show the UI until sites have loaded

--- src/renderer/components/App.svelte
+++ src/renderer/components/App.svelte
@@ -25,6 +25,8 @@
   import PageEditor from "./PageEditor";
   import BlockEditor from "./BlockEditor";
 
+  let loaded = false;
+
   onMount(async () => {
     $sites = await loadAllSites();
     // Load the first site
@@ -32,6 +34,7 @@
     $activeSite = await loadSite($sites[0]);
     $definitions = await loadDefinitionFiles();
     $sections = buildSections($activeSite, $definitions);
+    loaded = true;
   });
 </script>
 
@@ -53,51 +56,56 @@
 </style>
 
 <div class="body-wrapper">
-  <header>
-    <SiteHeader />
-  </header>
-  <main>
-    {#if $sites.length && !$creatingSite}
-      <div class="side-bar-container">
-        <SideBar />
-      </div>
-    {/if}
-    <div class="editor-container">
-
-      {#if !$sites.length && !$creatingSite}
-        <Intro />
-      {:else if $creatingSite}
-        <NewSiteWizard />
-      {:else if $activeSection && $activeSection.type === 'title'}
-        <HelpSection section={$activeSection.text} />
-      {:else if $activeSection && $activeSection.type === 'settings'}
-        <SettingsEditor
-          name={$activeSection.name}
-          definition={$activeSection.definition}
-          data={$activeSection.data}
-          @save="saveSettings(activeSection)"
-          @cancel="cancelSettings(activeSection)" />
-      {:else if $activeSection && $activeSection.type === 'collection'}
-        <CollectionEditor
-          name={$activeSection.text}
-          data={$activeSection.data}
-          @save="saveData(activeSection)"
-          @cancel="cancelData(activeSection)" />
-      {:else if $activeSection && $activeSection.type === 'page'}
-        <PageEditor
-          page={$activeSection.page}
-          definition={$activeSection.definition}
-          data={$activeSection.data}
-          @save="savePage(activeSection)"
-          @cancel="cancelPage(activeSection)" />
-      {:else if $activeSection && $activeSection.type === 'block'}
-        <BlockEditor
-          block={$activeSection.block}
-          definition={$activeSection.definition}
-          data={$activeSection.data}
-          @save="saveBlock(activeSection)"
-          @cancel="cancelBlock(activeSection)" />
+  {#if loaded}
+    <header>
+      <SiteHeader />
+    </header>
+    <main>
+      {#if $sites.length && !$creatingSite}
+        <div class="side-bar-container">
+          <SideBar />
+        </div>
       {/if}
+      <div class="editor-container">
+        {#if !$sites.length && !$creatingSite}
+          <Intro />
+        {:else if $creatingSite}
+          <NewSiteWizard />
+        {:else if $activeSection && $activeSection.type === 'title'}
+          <HelpSection section={$activeSection.text} />
+        {:else if $activeSection && $activeSection.type === 'settings'}
+          <SettingsEditor
+            name={$activeSection.name}
+            definition={$activeSection.definition}
+            data={$activeSection.data}
+            @save="saveSettings(activeSection)"
+            @cancel="cancelSettings(activeSection)" />
+        {:else if $activeSection && $activeSection.type === 'collection'}
+          <CollectionEditor
+            name={$activeSection.text}
+            data={$activeSection.data}
+            @save="saveData(activeSection)"
+            @cancel="cancelData(activeSection)" />
+        {:else if $activeSection && $activeSection.type === 'page'}
+          <PageEditor
+            page={$activeSection.page}
+            definition={$activeSection.definition}
+            data={$activeSection.data}
+            @save="savePage(activeSection)"
+            @cancel="cancelPage(activeSection)" />
+        {:else if $activeSection && $activeSection.type === 'block'}
+          <BlockEditor
+            block={$activeSection.block}
+            definition={$activeSection.definition}
+            data={$activeSection.data}
+            @save="saveBlock(activeSection)"
+            @cancel="cancelBlock(activeSection)" />
+        {/if}
+      </div>
+    </main>
+  {:else}
+    <div class="editor-container">
+      <p class="text-center">Loading sites...</p>
     </div>
-  </main>
+  {/if}
 </div>
